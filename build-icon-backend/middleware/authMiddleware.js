fe7aa989398381017dcb753eef06c5860b8cba7b
prajakt <prajaktb159@gmail.com>
2026-08-01 23:55:56 +0530

    Fix JWT secret

--- build-icon-backend/middleware/authMiddleware.js
+++ build-icon-backend/middleware/authMiddleware.js
@@ -1,45 +1,43 @@
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = "buildicon_secret_key";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 function verifyToken(req, res, next) {
 
-    const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-        return res.status(401).json({
-            message: "Access Denied"
-        });
-    }
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "Access Denied"
+    });
+  }
 
-    const token = authHeader.split(" ")[1];
+  const token = authHeader.split(" ")[1];
 
-    if (!token) {
-        return res.status(401).json({
-            message: "Token Missing"
-        });
-    }
+  if (!token) {
+    return res.status(401).json({
+      message: "Token Missing"
+    });
+  }
 
-    try {
+  try {
 
-        const decoded = jwt.verify(
-            token,
-            JWT_SECRET
-        );
+    const decoded = jwt.verify(
+      token,
+      JWT_SECRET
+    );
 
-        req.admin = decoded;
+    req.admin = decoded;
 
-        next();
+    next();
 
-    }
+  } catch (err) {
 
-    catch (err) {
+    return res.status(403).json({
+      message: "Invalid Token"
+    });
 
-        return res.status(403).json({
-            message: "Invalid Token"
-        });
-
-    }
+  }
 
 }
 

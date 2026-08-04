6581a8a2a5e6fee1b5a7276478b3c9563910c8c1
prajakt <prajaktb159@gmail.com>
2026-08-04 16:23:06 +0530

    temp server

--- build-icon-backend/server.js
+++ build-icon-backend/server.js
@@ -1,99 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const db = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
-// const analyticsRoutes = require("./routes/analyticsRoutes");
-
-
 const app = express();
 
-const PORT = process.env.PORT ||  5002;
-
-// ==========================
-// MIDDLEWARE
-// ==========================
-
-app.use(cors());
-
-app.use(express.json());
-
-// ==========================
-// STATIC UPLOADS
-// ==========================
-
-app.use(
-  "/uploads",
-  express.static(
-    path.join(__dirname, "uploads")
-  )
-);
-
-// ==========================
-// ROUTES
-// ==========================
-
-const projectStatusRoutes = require(
-  "./routes/projectStatusRoutes"
-);
-
-const galleryRoutes = require(
-  "./routes/galleryRoutes"
-);
-
-const developersRoutes = require(
-  "./routes/developersRoutes"
-);
-
-
-// ==========================
-// API ROUTES
-// ==========================
-
-app.use(
-  "/project-status",
-  projectStatusRoutes
-);
-
-app.use(
-  "/gallery",
-  galleryRoutes
-);
-
-app.use(
-  "/developers",
-  developersRoutes
-);
-
-// app.use(
-//   "/analytics", 
-//   analyticsRoutes
-// );
-
-app.use("/auth", authRoutes);
-
-// ==========================
-// HOME ROUTE
-// ==========================
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-
-  res.send(
-    "Build Icon Backend Working 🚀"
-  );
-
+  res.send("Backend Working 🚀");
 });
 
-// ==========================
-// SERVER START
-// ==========================
-
 app.listen(PORT, () => {
-
-  console.log(
-    `🚀 Server running on http://localhost:${PORT}`
-  );
-
+  console.log(`Server running on port ${PORT}`);
 });

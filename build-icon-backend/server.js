2488c36793918339546bc62768a82eebfbe5559f
prajakt <prajaktb159@gmail.com>
2026-08-04 17:52:51 +0530

    Remove duplicate PORT declaration

--- build-icon-backend/server.js
+++ build-icon-backend/server.js
@@ -10,8 +10,6 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-const PORT = process.env.PORT ||  5002;
-
 // ==========================
 // MIDDLEWARE
 // ==========================
